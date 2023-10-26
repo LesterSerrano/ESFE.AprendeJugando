ebcb22b984b1342830097101ab6de524d3b3751f
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2023-10-26 09:48:23 -0600

    cambios en la parte del juego

---
+++ ESFE.AprendeJugando/Models/Pregunta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESFE.AprendeJugando.Models
+{
+    public class Pregunta
+    {
+        public string PreguntaText { get; set; }
+        public List<string> OpcionesRespuesta { get; set; }
+        public int RespuestaCorrecta { get; set; }
+    }
+}
